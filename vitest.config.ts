4b3cab3ee518f37d4d6280156e1ebf3c3370884d
Steve Kinney <hello@stevekinney.net>
2023-03-22 13:19:57 -0500

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       ['**/*.component.test.ts', 'jsdom'],
     ],
     coverage: {
-      statements: 61.16,
+      statements: 50,
       thresholdAutoUpdate: true,
       include: ['src/**/*'],
       exclude: [
