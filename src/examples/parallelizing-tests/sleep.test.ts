39c110edfa48712555ddc342886cd52e18f45b62
Andy Lan <andy.lan@netscout.com>
2023-07-21 14:24:22 +0800

    describe.concurrent

--- src/examples/parallelizing-tests/sleep.test.ts
+++ src/examples/parallelizing-tests/sleep.test.ts
@@ -3,12 +3,37 @@ import { describe, it } from 'vitest';
 const sleep = (time = 1000) => {
   return new Promise<void>((resolve) => {
     setTimeout(() => {
+      console.log(time);
       resolve();
     }, time);
   });
 };
 
-describe.concurrent('sleep', () => {
+//run in series
+describe('sleep', () => {
+  it('should sleep for 500ms', async () => {
+    await sleep(500);
+    expect(true).toBe(true);
+  });
+
+  it('should sleep for 750ms', async () => {
+    await sleep(750);
+    expect(true).toBe(true);
+  });
+
+  it('should sleep for 1000ms', async () => {
+    await sleep(1000);
+    expect(true).toBe(true);
+  });
+
+  it('should sleep for 1500ms', async () => {
+    await sleep(1500);
+    expect(true).toBe(true);
+  });
+});
+
+//run in concurrent
+describe.concurrent('concurrent sleep', () => {
   it('should sleep for 500ms', async ({ expect }) => {
     await sleep(500);
     expect(true).toBe(true);
