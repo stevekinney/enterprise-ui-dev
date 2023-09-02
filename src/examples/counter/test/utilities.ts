61cb9ba103bb2424cf1a7af34d6bbb9a289f64ee
John Hunter <john@johnhunter.info>
2023-09-02 15:56:36 +0100

    Add Counter test
    - And extract a render util

--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,5 +1,14 @@
-export {};
+import { render as renderComponent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+type Args = Parameters<typeof renderComponent>;
 
 /**
- * For a complete example, see: test/utilities.ts
+ * Wraps the RTL render function with a userEvent user
  */
+export const render = (ui: Args[0], options?: Args[1]) => {
+  return {
+    ...renderComponent(ui, options),
+    user: userEvent.setup(),
+  };
+};
