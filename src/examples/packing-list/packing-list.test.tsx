6ec9aa08b5616ebaf3c2ef7cf9eaa7cfba105e27
John Hunter <john@johnhunter.info>
2023-09-03 15:17:38 +0100

    Add packing-list tests

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from 'test/utilities';
+import { render, screen, within } from 'test/utilities';
 import PackingList from '.';
 
 it('renders the Packing List application', () => {
@@ -10,19 +10,58 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText('New Item Name');
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  render(<PackingList />);
+  const field = screen.getByLabelText('New Item Name');
+  const button = screen.getByRole('button', { name: 'Add New Item' });
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+  expect(field).toHaveValue('');
+  expect(button).toBeDisabled();
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { user } = render(<PackingList />);
+  const field = screen.getByLabelText('New Item Name');
+  const button = screen.getByRole('button', { name: 'Add New Item' });
+
+  await user.click(field);
+  await user.keyboard('Foo');
+
+  expect(field).toHaveValue('Foo');
+  expect(button).not.toBeDisabled();
+});
+
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const field = screen.getByLabelText('New Item Name');
+  const button = screen.getByRole('button', { name: 'Add New Item' });
+  const unpackedList = screen.getByTestId('unpacked-items-list');
+
+  expect(within(unpackedList).queryByLabelText('Foo')).not.toBeInTheDocument();
+
+  await user.type(field, 'Foo');
+  await user.click(button);
+
+  within(unpackedList).getByLabelText('Foo');
+});
+
+// Note: dependent on side effect from previous test
+it.skip('removes an item from the unpacked item list when the clicking "Remove"', async () => {
+  const { user } = render(<PackingList />);
+  const field = screen.getByLabelText('New Item Name');
+  const addButton = screen.getByRole('button', { name: 'Add New Item' });
+  const unpackedList = screen.getByTestId('unpacked-items-list');
+
+  within(unpackedList).getByLabelText('Foo');
+
+  const removeButton = screen.getByLabelText('Remove Foo');
+
+  await user.click(removeButton);
+
+  expect(within(unpackedList).queryByLabelText('Foo')).not.toBeInTheDocument();
+});
