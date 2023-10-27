78fa5659dfabbcc7e4603338596e593f07ebaed0
Deyan Rumenov <drumenov@tutamail.com>
2023-10-27 15:46:52 +0300

    Some exercise done.

--- src/examples/great-expectations/exercise.test.ts
+++ src/examples/great-expectations/exercise.test.ts
@@ -12,48 +12,54 @@ import { KanbanBoard } from '$lib/kanban-board';
  * toThrowError: https://vitest.dev/api/expect.html#tothrowerror
  */
 
-it.todo(
+it(
   'should pass if the two numbers would add up correctly in a language other than JavaScript',
   () => {
-    expect(0.2 + 0.1).toEqual(0.3);
+    expect(0.2 + 0.1).toBeCloseTo(0.3);
   },
 );
 
 describe('createPerson', () => {
-  it.todo('should create an instance of a person', () => {
+  it('should create an instance of a person', () => {
     const person = createPerson('Ada Lovelace');
     expect.hasAssertions();
-    // Verify that person is an instance of a Person.
+    expect(person).toBeInstanceOf(Person);
   });
 });
 
 describe('Kanban Board', () => {
-  it.todo('should include "Backlog" in board.statuses', () => {
+  it('should include "Backlog" in board.statuses', () => {
     const board = new KanbanBoard('Things to Do');
     expect.hasAssertions();
-    // Verify that board.statuses contains "Backlog".
+    expect(board.statuses).toContain("Backlog");
   });
 
-  it.todo('should *not* include "Bogus" in board.statuses', () => {
+  it('should *not* include "Bogus" in board.statuses', () => {
     const board = new KanbanBoard('Things to Do');
     expect.hasAssertions();
-    // Verify that board.statuses does not contain "Bogus".
+    expect(board.statuses).not.toContain("Bogus");
   });
 
-  it.todo(
+  it(
     'should include an added status in board.statuses using #addStatus',
     () => {
       const board = new KanbanBoard('Things to Do');
       expect.hasAssertions();
       // Use board.addStatus to add a status.
       // Verify that the new status is—in fact—now in board.statuses.
+      board.addStatus("new");
+      expect(board.statuses).toContain("new");
     },
   );
 
-  it.todo('should remove a status using #removeStatus', () => {
+  it('should remove a status using #removeStatus', () => {
     const board = new KanbanBoard('Things to Do');
     expect.hasAssertions();
     // Use board.removeStatus to remove a status.
+    board.addStatus("new");
+    expect(board.statuses).toContain("new");
+    board.removeStatus("new");
+    expect(board.statuses).not.toContain("new");
 
     // You can be clever or you can just assume "Backlog" is in board.statuses
     // by default.
@@ -63,25 +69,28 @@ describe('Kanban Board', () => {
 });
 
 describe('Person', () => {
-  it.todo('will create a person with a first name', () => {
+  it('will create a person with a first name', () => {
     const person = new Person('Madonna');
     expect.hasAssertions();
     // Verify that person.firstName is correct.
+    expect(person.firstName).toBe('Madonna');
   });
 
-  it.todo('will create a person with a first and last name', () => {
+  it('will create a person with a first and last name', () => {
     const person = new Person('Madonna Cicone');
     expect.hasAssertions();
     // Verify that person.lastName is correct.
+    expect(person.lastName).toBe("Cicone");
   });
 
-  it.todo('will create a person with a first, middle, and last name', () => {
+  it('will create a person with a first, middle, and last name', () => {
     const person = new Person('Madonna Louise Cicone');
     expect.hasAssertions();
     // Verify that person.middleName is correct.
+    expect(person.middleName).toBe('Louise');
   });
 
-  it.todo('will throw if you provide an empty string', () => {
+  it('will throw if you provide an empty string', () => {
     const fn = () => {
       new Person('');
     };
@@ -89,9 +98,10 @@ describe('Person', () => {
     expect.hasAssertions();
 
     // Verify that function above throws.
+    expect(fn).toThrow();
   });
 
-  it.todo(
+  it(
     'will throw a specific error message if you provide an empty string',
     () => {
       const errorMessage = 'fullName cannot be an empty string';
@@ -103,10 +113,11 @@ describe('Person', () => {
       expect.hasAssertions();
 
       // Verify that function above throws the error message above.
+      expect(fn).toThrowError(errorMessage);
     },
   );
 
-  it.todo('will add a friend', () => {
+  it('will add a friend', () => {
     const john = new Person('John Lennon');
     const paul = new Person('Paul McCartney');
 
@@ -115,9 +126,10 @@ describe('Person', () => {
     expect.hasAssertions();
 
     // Verify that john.friends contains paul.
+    expect(john.friends).toContain(paul);
   });
 
-  it.todo('will mutually add a friend', () => {
+  it('will mutually add a friend', () => {
     const john = new Person('John Lennon');
     const paul = new Person('Paul McCartney');
 
@@ -126,9 +138,10 @@ describe('Person', () => {
     expect.hasAssertions();
 
     // Verify that paul.friends contains john.
+    expect(paul.friends).toContain(john);
   });
 
-  it.todo('will remove a friend', () => {
+  it('will remove a friend', () => {
     const john = new Person('John Lennon');
     const paul = new Person('Paul McCartney');
 
@@ -138,9 +151,10 @@ describe('Person', () => {
     expect.hasAssertions();
 
     // Verify that john.friends does not inclide paul.
+    expect(john.friends).not.toContain(paul);
   });
 
-  it.todo('will mutually remove friends', () => {
+  it('will mutually remove friends', () => {
     const john = new Person('John Lennon');
     const paul = new Person('Paul McCartney');
 
@@ -150,6 +164,7 @@ describe('Person', () => {
     expect.hasAssertions();
 
     // Verify that paul.friends does not include john.
+    expect(paul.friends).not.toContain(john);
   });
 });
 
@@ -158,11 +173,11 @@ const explode = () => {
 };
 
 describe('explode', () => {
-  it.todo('should throw an error', () => {
-    explode();
+  it('should throw an error', () => {
+    expect(explode).toThrow();
   });
 
-  it.todo('should throw a specific error containing "terribly wrong"', () => {
-    explode();
+  it('should throw a specific error containing "terribly wrong"', () => {
+    expect(explode).toThrow(/.terribly wrong$/);
   });
 });
--- src/examples/great-expectations/items-slice.test.ts
+++ src/examples/great-expectations/items-slice.test.ts
@@ -10,22 +10,25 @@ it('returns an empty array as the initial state', () => {
   expect(reducer(undefined, { type: 'noop' })).toEqual([]);
 });
 
-it.todo('supports adding an item with the correct name', () => {
+it('supports adding an item with the correct name', () => {
   expect.hasAssertions();
   const result = reducer([], add({ name: 'iPhone' }));
+  expect(result).toMatchObject([{name: 'iPhone' }]);
 });
 
-it.todo('prefixes ids with "item-"', () => {
+it('prefixes ids with "item-"', () => {
   expect.hasAssertions();
   const result = reducer([], add({ name: 'iPhone' }));
+  expect(result[0].id).toMatch(/item-*/);
 });
 
-it.todo('defaults new items to a packed status of false', () => {
+it('defaults new items to a packed status of false', () => {
   expect.hasAssertions();
   const result = reducer([], add({ name: 'iPhone' }));
+  expect(result).toMatchObject([{packed: false}]);
 });
 
-it.todo('supports removing an item', () => {
+it('supports removing an item', () => {
   expect.hasAssertions();
   const state = [
     {
@@ -36,9 +39,11 @@ it.todo('supports removing an item', () => {
   ];
 
   const result = reducer(state, remove({ id: '1' }));
+
+  expect(result).toMatchObject([]);
 });
 
-it.todo('supports toggling an item', () => {
+it('supports toggling an item', () => {
   expect.hasAssertions();
   const state = [
     {
@@ -49,9 +54,11 @@ it.todo('supports toggling an item', () => {
   ];
 
   const result = reducer(state, toggle({ id: '1' }));
+
+  expect(result).toMatchObject([{packed: true}]);
 });
 
-it.todo('supports updating an item', () => {
+it('supports updating an item', () => {
   expect.hasAssertions();
   const state = [
     {
@@ -65,9 +72,11 @@ it.todo('supports updating an item', () => {
     state,
     update({ id: '1', name: 'Samsung Galaxy S23' }),
   );
+
+  expect(result).toMatchObject([{id: '1', name: 'Samsung Galaxy S23'}]);
 });
 
-it.todo('supports marking all items as unpacked', () => {
+it('supports marking all items as unpacked', () => {
   expect.hasAssertions();
   const state = [
     {
@@ -83,4 +92,7 @@ it.todo('supports marking all items as unpacked', () => {
   ];
 
   const result = reducer(state, markAllAsUnpacked());
+
+  expect(result).toMatchObject([{packed: false}, {packed: false}]);
+
 });
