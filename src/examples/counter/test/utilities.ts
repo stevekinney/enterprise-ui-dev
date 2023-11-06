98217fa1bf52f451d89d9edd4fcd9de27d89042c
Kushal V. Mahajan <11805936+kushalmahajan@users.noreply.github.com>
2023-11-06 16:49:21 +0530

    setup and utilities function change

--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,4 +1,18 @@
-export {};
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { render as renderComponent } from '@testing-library/react';
+
+export const render = (
+  ui: React.ReactElement,
+  options?: Parameters<typeof renderComponent>[1],
+) => {
+  const user = userEvent.setup();
+  const result = renderComponent(ui, options);
+  return {
+    ...result,
+    user,
+  };
+};
 
 /**
  * For a complete example, see: test/utilities.ts
