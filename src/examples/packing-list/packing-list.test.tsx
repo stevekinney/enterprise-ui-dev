f336ce47d28105d9a550a71fa34973fd1f4dffca
Mario Andres <mrendon@anomali.com>
2024-02-08 22:19:14 -0600

    ad test for packing-list component

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -10,19 +10,31 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText(/new item name/i);
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  render(<PackingList />);
+  const addNewItemBtn = screen.getByRole('button', { name: /add new item/i });
+  expect(addNewItemBtn).toBeDisabled();
+});
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { user } = render(<PackingList />);
+  const input = screen.getByLabelText(/new item name/i);
+  const addNewItemBtn = screen.getByRole('button', { name: /add new item/i });
+  await user.type(input, 'New Item');
+  expect(addNewItemBtn).toBeEnabled();
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const input = screen.getByLabelText(/new item name/i);
+  const addNewItemBtn = screen.getByRole('button', { name: /add new item/i });
+  await user.type(input, 'ITEM FROM TEST');
+  await user.click(addNewItemBtn);
+  const unpackedItemsList = screen.getByTestId('unpacked-items');
+  expect(unpackedItemsList).toHaveTextContent('ITEM FROM TEST');
+});
